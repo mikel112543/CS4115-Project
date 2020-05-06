373d0da4d9418834f3f6836da4bcb144a5ffb8a2
Michael Danaher <michael.danaher112543@gmail.com>
2020-05-06 02:59:03 +0100

    - Added functionality to read in both files - Created vector to hold lists of neighbours

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <list>
 
 using namespace std;
 
@@ -32,35 +33,106 @@ public:
         z = _z;
     }
 
+
 private:
     float x;
     float y;
     float z;
 };
 
-int main() {
-
-    vector<coordinates> coordinatesList;
-
-    float x;
-    float y;
-    float z;
-    ifstream indata;
-    indata.open(R"(C:\Users\mikel\OneDrive - University of Limerick\2nd Year\Semester 2\CS4115 (Data Structures and Algorithms)\Project\belgium.osm.xyz)");
-    if(!indata) {
-        cout << "Error: file could not be opened" << endl;
-        exit(1);
-    }else {
-        cout << "File read successfully" << endl;
+class Node {
+public:
+    Node(int _parent, int _neighbour) {
+        this->parent = _parent;
+        this->neighbour = _neighbour;
     }
-    while(!indata.eof()) {
-            indata >> x;
-            indata >> y;
-            indata >> z;
+    int getParent() { return parent; }
+    int getNeighbour() { return neighbour; }
+
+
+private:
+    int parent;
+    int neighbour;
+
+
+};
+
+
+int main() {
+    string xyzPath;
+    string graphPath;
+    vector<coordinates> coordinatesList;
+    typedef list<Node> neighbours;
+    vector<neighbours> collection;
+
+    string file;
+    string graph = ".osm.graph";
+    string xyz = ".osm.xyz";
+    cin >> file;
+    string temp = file;
+    if(file.rfind(".osm.graph") != true) {
+        graphPath = file.append(graph);
+    }if(file.rfind(".osm.xyz") != true) {
+        xyzPath = temp.append(xyz);
+    }
+        ifstream xyzFile;
+        xyzFile.open(xyzPath.c_str());
+        if(!xyzFile) {
+            cout << "Error: file could not be opened" << endl;
+            exit(1);
+        }else {
+            cout << "File read successfully" << endl;
+        }
+        while(!xyzFile.eof()) {
+            float x, y, z;
+            xyzFile >> x;
+            xyzFile >> y;
+            xyzFile >> z;
             coordinates node(x, y, z);
             coordinatesList.push_back(node);
-    }
-    indata.close();
-    cout << coordinatesList[1441294].getY() << endl;
+        }
+        xyzFile.close();
+        cout << "Finished reading file" << endl;
 
+
+
+    ifstream graphFile;
+    string line;
+    int numberOfNeighbours = 0;
+    int lineNum = 0;
+    graphFile.open(graphPath.c_str());
+    if(!graphFile) {
+        cout << "Error: file could not be opened" << endl;
+        exit(1);
+    }else{
+        cout << "File read successfully" << endl;
+    }
+    while(!graphFile.eof()) {
+        while(getline(graphFile,line)) {
+            std::string::size_type n = line.find('%');
+            if (n != std::string::npos) {
+                continue;
+            }
+            istringstream num(line);
+            for(int i = 0; line[i] != '\0'; i++) {
+                if(line[i] == ' ') {
+                    numberOfNeighbours++;
+                }
+            }
+            numberOfNeighbours+=1;
+            for(int i = 0; i < numberOfNeighbours; i++) {
+                int temp;
+                num >> temp;
+                //Node node(lineNum, temp);
+                neighbours neighbours1;
+                neighbours1.push_back(Node(lineNum, temp));
+                collection.push_back(neighbours1);
+                //Node inverse(temp, lineNum);
+                //neighbours.push_back(inverse);
+            }
+            lineNum++;
+            numberOfNeighbours = 0;
+        }
+    }
+    cout << "Finished reading file" << endl;
 }
