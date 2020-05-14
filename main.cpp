0ed3126e36e2109557ff8078f575ee79f5cb843e
Michael Danaher <michael.danaher112543@gmail.com>
2020-05-14 04:23:41 +0100

    - Queries 1-5 completed - Queries 6 & 7 no started (maybe later date); - Key takeaway - References

--- main.cpp
+++ main.cpp
@@ -40,53 +40,57 @@ class Node {
     int neighbour;
     float distance;
 public:
-    Node();
 
     Node(int _neighbour, float _distance) : neighbour(_neighbour), distance(_distance) {}
 
-    int getNeighbour() { return neighbour; }
-
-    float getDistance(vector<coordinates *> vec, int parent, int neighbour) {
-        float x1 = vec[parent - 1]->getX();
-        float y1 = vec[parent - 1]->getY();
-        float x2 = vec[neighbour - 1]->getX();
-        float y2 = vec[neighbour - 1]->getY();
-        distance = sqrt(pow(x2 - x1, 2) +
-                        pow(y2 - y1, 2) * 1.0);
-
-        return distance;
-    }
-
-    void setDistance(float _distance) { distance = _distance; }
+    int getNeighbour() const { return neighbour; }
 
 private:
 
 };
 
+typedef vector<int> numVec;
+typedef vector<numVec> adjMat;
 typedef list<Node *> neighbours;
 vector<coordinates *> coordinatesList;
+typedef vector<neighbours> collection;
 
-vector<vector<int>> matPow(vector<vector<int>> mat1, vector<vector<int>> &mat2, int pow) {
-    vector<vector<int>> mult(mat1.size(), vector<int>(mat1.size(), 0));
+void createAdjMat(adjMat &m1, const collection &collection1) {
+    for (auto &i : collection1) {
+        numVec numVec1(collection1.size());
+        for (Node *node : i) {
+            int num = node->getNeighbour();
+            if (num != 0) {
+                numVec1[num - 1] = 1;
+            }
+        }
+        m1.push_back(numVec1);
+    }
+}
+
+void matPow(const adjMat m1, adjMat &m2, adjMat &temp, adjMat &mult, int pow) {
     int i = 1;
     while (i < pow) {
-        for (int r = 0; r < mat1.size(); r++) {
-            for (int c = 0; c < mat1[r].size(); c++) {
-                for (int k = 0; k < mat1[r].size(); k++) {
-                    mult[r][c] += mat1[r][k] * mat2[k][c];
+        for (unsigned int c = 0; c < m1.size(); c++) {
+            numVec vec(m1.size(), 0);
+            temp.push_back(vec);
+            mult.push_back(vec);
+        }
+        for (int r = 0; r < m1.size(); r++) {
+            for (int c = 0; c < m1[r].size(); c++) {
+                for (int k = 0; k < m1[r].size(); k++) {
+                    temp[r][c] += m1[r][k] * m2[k][c];
                 }
             }
         }
-        mat2 = mult;
-        mult = vector<vector<int>>(mat2.size(), vector<int>(mat2.size(), 0));
+        mult = temp;
+        m2 = temp;
+        temp.clear();
         i++;
     }
-    /*for (int r = 0; r < mult.size(); r++) {
-        for (int c = 0; c < mult.size(); c++) {
-            std::cout << mat2[r][c] << " ";
-        }
-    }*/
-    return mat2;
+    if (pow == 1) {
+        mult = m1;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -94,7 +98,7 @@ int main(int argc, char *argv[]) {
     int m;
     string xyzPath;
     string graphPath;
-    vector<neighbours> collection;
+    collection collection1;
 
     string file;
     string graph = ".osm.graph";
@@ -113,9 +117,9 @@ int main(int argc, char *argv[]) {
     if (!xyzFile) {
         cout << "Error: file could not be opened" << endl;
         exit(1);
-    } else {
+    } /*else {
         cout << "File read successfully" << endl;
-    }
+    }*/
     while (!xyzFile.eof()) {
         float x, y, z;
         xyzFile >> x;
@@ -125,7 +129,7 @@ int main(int argc, char *argv[]) {
         coordinatesList.push_back(coordinate);
     }
     xyzFile.close();
-    cout << "Finished reading file" << endl;
+    //cout << "Finished reading file" << endl;
 
     //Read in .graph file
     ifstream graphFile;
@@ -135,9 +139,9 @@ int main(int argc, char *argv[]) {
     if (!graphFile) {
         cout << "Error: file could not be opened" << endl;
         exit(1);
-    } else {
+    } /*else {
         cout << "File read successfully" << endl;
-    }
+    }*/
     while (!graphFile.eof()) {
         while (getline(graphFile, line)) {
             std::string::size_type t = line.find('%');
@@ -154,7 +158,7 @@ int main(int argc, char *argv[]) {
                 node = new Node(0, 0);          //Record nodes with zero neighbours
                 neighbours neighbours1;
                 neighbours1.push_back(node);
-                collection.push_back(neighbours1);
+                collection1.push_back(neighbours1);
                 lineNum++;
             } else {
                 neighbours neighbours1;                              //Create List of neighbours for line number
@@ -166,108 +170,90 @@ int main(int argc, char *argv[]) {
                     node = new Node(neighbour, coordinate->getDistance(coordinatesList, lineNum, neighbour));
                     neighbours1.push_back(node);                    //Add neighbours to list
                 }
-                collection.push_back(neighbours1);                  //Add List to vector
+                collection1.push_back(neighbours1);                  //Add List to vector
                 lineNum++;
             }
         }
     }
     graphFile.close();
-    cout << "Finished reading file" << endl;
+    //cout << "Finished reading file" << endl;
 
     int query;
     cin >> query;
     switch (query) {
         case 1: {
             cout << "n= " << n << " m= " << m << endl;
-            break;
+            exit(1);
         }
         case 2: {
-            int size = (collection[0].size());
+            int size = (collection1[0].size());
             int index = 1;
-            for (int i = 0; i < collection.size(); i++) {
-                if (size < collection[i].size()) {
+            for (int i = 0; i < collection1.size(); i++) {
+                if (size < collection1[i].size()) {
                     index = i + 1;
-                    size = collection[i].size();
+                    size = collection1[i].size();
                 }
             }
             cout << "v= " << index << " |N(v)|= " << size;
-            break;
+            exit(1);
         }
         case 3: {
             float totalNeighbours = 0.0;
-            for (auto &i : collection) {
+            for (auto &i : collection1) {
                 totalNeighbours += i.size();
             }
-            float average = totalNeighbours / collection.size();
-            cout << "avg |N(v)|= " << average << setprecision(6);
-            break;
+            float average = totalNeighbours / collection1.size();
+            cout << "avg |N(v)|= " << fixed << average << setprecision(6);
+            exit(1);
         }
         case 4: {
             int node;
             cin >> node;
             cout << "N(" << node << ")= ";
-            for (const auto &neighbour : collection[node - 1]) {
+            for (const auto &neighbour : collection1[node - 1]) {
                 cout << neighbour->getNeighbour() << " ";
             }
-            break;
+            exit(1);
         }
         case 5: {
             int parent;
             int distance;
             cin >> parent;
             cin >> distance;
-            vector<vector<int>> adjMat(n, vector<int>(n, 0));
-            for (int i = 0; i < collection.size() / 2; i++) {
-                for (Node *node : collection[i]) {
-                    int num = node->getNeighbour();
-                    if (num != 0) {
-                        adjMat.at(i).at(num - 1) = 1;
-                        adjMat.at(num - 1).at(i) = 1;
-                    }
-                }
-            }
-            for (auto &row : adjMat) {
-                for (int &col : row) {
-                    std::cout << col << ' ';
-                }
-            }
+            adjMat adjMat1;
+            adjMat adjMat2;
+            adjMat tempMat;
+            adjMat res;
+            createAdjMat(adjMat1, collection1);
+            createAdjMat(adjMat2, collection1);
             cout << "N(" << parent << "," << distance << ")=";
-            for(int i = 0; i < matPow(adjMat, adjMat, distance).size(); i++) {
-                if(matPow(adjMat, adjMat, 4).at(parent-1).at(i) != 0) {
+            matPow(adjMat1, adjMat2, tempMat, res, distance);
+            for (int i = 0; i < res.size(); i++) {
+                if (res.at(parent - 1).at(i) != 0) {
                     cout << i + 1 << " ";
                 }
             }
+            exit(0);
+        }
+        case 6: {
+            int start;
+            int finish;
+            cin >> start;
+            cin >> finish;
+            cout << "Path: " << start;
+            cout << "d(" << start << "," << finish << ")=";
 
-            /* string nodes;
-             getline(std::cin,nodes);
-             getline(std::cin,nodes);
-             istringstream num(nodes);
-             string userInputNodes;
-             vector<int> vec;
-             int node;
-             while (!(num.eof())) {
-                 num >> node;
-                 //while(cin >> node){
-                 userInputNodes += to_string(node) + ",";
-                 //list<Node*>::const_iterator rit;
-                 for (const auto &neighbour : collection[node - 1]) {
-                     //for(rit = collection[node-1].begin(); rit != collection[node-1].end(); rit++) {
-                     //Node *node1 = *rit;
-                     vec.push_back(neighbour->getNeighbour());
-                 }
-             }
-             sort(vec.begin(), vec.end());
-             for (int i = 0; i < vec.size()-1; i++) {
-                 if (vec[i] == vec[i + 1]) {
-                     vec.erase(vec.begin() + i);
-                 }
-             }
-             cout << "N(" << userInputNodes << ")= ";
-             for (int neighbour : vec) {
-                 cout << neighbour << " ";
-             }
-             break;*/
-            break;
+            exit(0);
+        }
+        case 7: {
+            int start;
+            int finish;
+            cin >> start;
+            cin >> finish;
+            cout << "Path: " << start;
+            cout << "ed(" << start << "," << finish << ")=";
+
+            exit(0);
         }
     }
 }
